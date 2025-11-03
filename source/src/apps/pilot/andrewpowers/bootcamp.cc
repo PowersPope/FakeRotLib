43314c2be2de6e1ae05c3c193b5019ab44906e2f
PowersPope <apowers4@uoregon.edu>
2025-11-03 13:55:03 -0800

    <FEAT> Added in a pose_from_pdb statement that reads in our passed pdb file and saves it into a PoseOP object

--- source/src/apps/pilot/andrewpowers/bootcamp.cc
+++ source/src/apps/pilot/andrewpowers/bootcamp.cc
@@ -11,12 +11,16 @@
 #include <src/basic/options/option.hh>
 #include <basic/options/keys/in.OptionKeys.gen.hh>
 #include <devel/init.hh>
+#include <src/core/pose/Pose.fwd.hh>
+#include <src/core/import_pose/import_pose.hh>
+#include <utility/pointer/owning_ptr.hh>
 
 int main( int argc, char ** argv) {
 	devel::init( argc, argv );
 	utility::vector1< std::string > filenames = basic::options::option[basic::options::OptionKeys::in::file::s ].value();
 	if ( filenames.size() > 0 ) {
 		std::cout << "You entered: " << filenames[1] << " as the PDB file to be read" << std::endl;
+		core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
 	} else {
 		std::cout << "You didn't provide a PDB file with the -in::file::s option" << std::endl;
 		return 1;
