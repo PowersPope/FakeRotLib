57538f59f1f2ad8f4fac29927634dadcae12d24f
PowersPope <apowers4@uoregon.edu>
2025-11-03 16:01:09 -0800

    <FEAT> MonteCarlo sampling added and check to see if  before adding in changes.

--- source/src/apps/pilot/andrewpowers/bootcamp.cc
+++ source/src/apps/pilot/andrewpowers/bootcamp.cc
@@ -12,16 +12,24 @@
 #include <basic/options/keys/in.OptionKeys.gen.hh>
 #include <devel/init.hh>
 #include <src/core/pose/Pose.fwd.hh>
+#include <src/core/pose/Pose.hh>
 #include <src/core/import_pose/import_pose.hh>
 #include <utility/pointer/owning_ptr.hh>
 #include <src/core/scoring/ScoreFunctionFactory.hh>
 #include <src/core/scoring/ScoreFunction.hh>
+#include <src/numeric/random/random.hh>
+#include <src/numeric/random/uniform.hh>
+#include <src/protocols/moves/MonteCarlo.hh>
+#include <src/core/conformation/Residue.hh>
 
 int main( int argc, char ** argv) {
 	// Init Rosetta as a whole
 	devel::init( argc, argv );
 	// Init our scorefunction
 	core::scoring::ScoreFunctionOP scorefxn = core::scoring::get_score_function("ref2015"); 
+	// Set our random number generator
+	numeric::random::rg().set_seed("mt19937", 192); 
+
 
 	// Check if we get a pdb
 	utility::vector1< std::string > filenames = basic::options::option[basic::options::OptionKeys::in::file::s ].value();
@@ -36,11 +44,34 @@ int main( int argc, char ** argv) {
 	// Load our file into a pose object
 	core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
 
+	// Setup our MonteCarlo object
+	protocols::moves::MonteCarlo  mc( *mypose, *scorefxn, 100 );
+
 	// Score our pose
-	core::Real score = scorefxn->score( * mypose );
+// 	core::Real score = scorefxn->score( * mypose );
 
 	// Output our score
-	std::cout << "File Score: " << score << std::endl;
+// 	std::cout << "File Score: " << score << std::endl;
+
+	
+	// Monte Carlo change residues
+	for ( int i=0; i<10; i++ ) {
+		core::Size size_pose = mypose->size();
+		core::Size randres = ( numeric::random::rg().uniform() * size_pose + 1 );
+		if ( mypose->residue( randres ).is_protein() ) {
+			core::Real pert1 = numeric::random::rg().gaussian();
+			core::Real pert2 = numeric::random::rg().gaussian();
+			core::Real orig_phi = mypose->phi( randres );
+			core::Real orig_psi = mypose->psi( randres );
+			mypose->set_phi( randres, orig_phi + pert1 );
+			mypose->set_psi( randres, orig_psi + pert2 );
+			mc.boltzmann( * mypose );
+			std::cout << "MC MOVEMENT!!! Round: " << i << std::endl;
+		}
+	}
+
+// 	std::cout << "Random Res: " << randres << " Pert1: " << orig_phi + pert1 << std::endl;
+// 	std::cout << "Random Res: " << randres << " Pert2: " << orig_psi + pert2 << std::endl;
 
 	return 0;
 }
