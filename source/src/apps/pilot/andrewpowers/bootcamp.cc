d20c543b5c86b66661147ef9f62c88bb38fff074
PowersPope <apowers4@uoregon.edu>
2025-11-05 13:14:01 -0800

    <FIX> Updated the Bootcamp.cc, so that the int / int division would return a float for the proption being reported. This finishes lab4

--- source/src/apps/pilot/andrewpowers/bootcamp.cc
+++ source/src/apps/pilot/andrewpowers/bootcamp.cc
@@ -7,7 +7,10 @@
 // (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
+#include <basic/Tracer.hh>
+
 #include <iostream>
+
 #include <src/basic/options/option.hh>
 #include <basic/options/keys/in.OptionKeys.gen.hh>
 #include <devel/init.hh>
@@ -25,14 +28,22 @@
 #include <src/core/pack/task/PackerTask.hh>
 #include <src/core/pack/task/TaskFactory.hh>
 #include <src/core/kinematics/MoveMap.hh>
+#include <src/core/kinematics/FoldTree.hh>
 #include <src/core/optimization/MinimizerOptions.hh>
 #include <src/core/optimization/AtomTreeMinimizer.hh>
+#include <core/pose/variant_util.hh>
+#include <protocols/bootcamp/fold_tree_from_ss.hh>
+#include <core/scoring/ScoreType.hh>
+
+
+static basic::Tracer TR( "apps.pilot.andrewpowers.bootcamp" );
 
 int main( int argc, char ** argv) {
 	// Init Rosetta as a whole
 	devel::init( argc, argv );
 	// Init our scorefunction
 	core::scoring::ScoreFunctionOP scorefxn = core::scoring::get_score_function("ref2015"); 
+	scorefxn->set_weight( core::scoring::ScoreType::linear_chainbreak, 1.0 );
 	// Set our random number generator
 	numeric::random::rg().set_seed("mt19937", 192); 
 	// Setup our Movemap
@@ -47,29 +58,28 @@ int main( int argc, char ** argv) {
 	utility::vector1< std::string > filenames = basic::options::option[basic::options::OptionKeys::in::file::s ].value();
 
 	if ( filenames.size() > 0 ) {
-		std::cout << "You entered: " << filenames[1] << " as the PDB file to be read" << std::endl;
+		TR << "You entered: " << filenames[1] << " as the PDB file to be read" << std::endl;
 	} else {
-		std::cout << "You didn't provide a PDB file with the -in::file::s option" << std::endl;
+		TR << "You didn't provide a PDB file with the -in::file::s option" << std::endl;
 		return 1;
 	}
 
 	// Load our file into a pose object
 	core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
 
+	// Generate our dssp sequence
+	core::kinematics::FoldTree ft = protocols::bootcamp::fold_tree_from_ss( *mypose );
+	mypose->fold_tree( ft );
+	correctly_add_cutpoint_variants( *mypose );
+
 	// Setup our MonteCarlo object
-	protocols::moves::MonteCarlo  mc( *mypose, *scorefxn, 100 );
+	protocols::moves::MonteCarlo  mc( *mypose, *scorefxn, 0.8 ); // 100 temp before
 
-	// Score our pose
-// 	core::Real score = scorefxn->score( * mypose );
-
-	// Output our score
-// 	std::cout << "File Score: " << score << std::endl;
-
-	
 	// Copy the pose and init values
 	core::pose::Pose copy_pose;
-	core::Size accepted_moves = 0;
-	core::Real score_overtime = 0;
+	bool bool_out;
+	core::Real accepted_moves = 0.0;
+	core::Real score_overtime = 0.0;
 	int steps = 4;
 
 	// Monte Carlo change residues 
@@ -94,15 +104,16 @@ int main( int argc, char ** argv) {
 			copy_pose = *mypose;
 			atm.run( copy_pose, mm, *scorefxn, min_opts );
 			*mypose = copy_pose;
-			accepted_moves += mc.boltzmann( *mypose );
+			bool_out = mc.boltzmann( *mypose );
+			accepted_moves += bool_out;
 			score_overtime += mc.last_score();
-			std::cout << "MonteCarlo Round: " << i << "Score of Move: " << mc.last_score() << std::endl;
+			TR << "MonteCarlo Round: " << i << " Score of Move: " << mc.last_score() << " Move Accepted: " << bool_out << std::endl;
 		}
 	}
 
-	std::cout << "Final Score output: " << mc.last_accepted_score() << std::endl;
-	std::cout << "Proportion of accepted moves: " << accepted_moves / steps << std::endl;
-	std::cout << "Average Score of protein: " << score_overtime / steps << std::endl;
+	TR << "Final Score output: " << mc.last_accepted_score() << std::endl;
+	TR << "Proportion of accepted moves: " << accepted_moves / steps << std::endl;
+	TR << "Average Score of protein: " << score_overtime / steps << std::endl;
 
 	return 0;
 }
